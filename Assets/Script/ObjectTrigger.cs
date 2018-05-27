0562d6248b02b7977e46b50987b1dfdb5efb2626
GYUNA KIM <kkn00077@gmail.com>
2018-05-27 23:41:54 +0900

    오브젝트 상호작용 코드 수정 및 exe 파일 생성

--- Assets/Script/ObjectTrigger.cs
+++ Assets/Script/ObjectTrigger.cs
@@ -2,66 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectTrigger : MonoBehaviour {
+public class ObjectTrigger : MonoBehaviour
+{
 
-    bool[] items=new bool[8]; //인덱스 0~3까지 귀신 쪽지, 나머지는 일반 아이템들
+    bool[] items = new bool[8]; //인덱스 0~3까지 귀신 쪽지, 나머지는 일반 아이템들
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-    void OnTriggerStay(Collider col) { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
         // 클릭했을 때 처리, 아이템일 경우와, 단순한 오브젝트일 경우
 
-       /* if (!col.gameObject.CompareTag("item")) // 아이템이 아닐경우
-        {
-            if (col.gameObject.CompareTag("Trigger")) // 아이템과 상호작용하는 오브젝트일 경우
-            {
-
-            }
-        }*/
-        
-        /*
-        if(col.gameObject.CompareTag("item")) //아이템일 경우
-        {
-            UnityEngine.Debug.Log("item");
-            if (Input.GetMouseButtonDown(0))
-            {
-                
-                items[int.Parse(col.gameObject.name)] = true; // 아이템의 이름을 인덱스로 설정해줘서 그 인덱스대로 아이템을 얻으면 true값을 넣어줌
-               col.gameObject.SetActive(false); // 아이템을 가지고 간 것처럼 보이기 위해 아이템 오브젝트를 화면상에서 그리지 않음
-
-               UnityEngine.Debug.Log(items[int.Parse(col.gameObject.name)]);
-            }
-        }*/
+        GameObject go = col.gameObject;
 
         if (Input.GetMouseButtonDown(0)) // 왼쪽 마우스 버튼 클릭 했을 때
         {
-            UnityEngine.Debug.Log("클릭됨");
 
-            if (col.gameObject.CompareTag("item")) // trigger가 일어난(충돌한) 오브젝트의 태그명이 item일 때
+            if (go.CompareTag("item")) // trigger가 일어난(충돌한) 오브젝트의 태그명이 item일 때
             {
-                items[int.Parse(col.gameObject.name)] = true; // 아이템의 이름을 인덱스로 설정해줘서 그 인덱스대로 아이템을 얻으면 true값을 넣어줌
-                col.gameObject.SetActive(false); // 아이템을 가지고 간 것처럼 보이기 위해 아이템 오브젝트를 화면상에서 그리지 않음
-                UnityEngine.Debug.Log(items[int.Parse(col.gameObject.name)]);
+                items[int.Parse(go.name)] = true; // 아이템의 이름을 인덱스로 설정해줘서 그 인덱스대로 아이템을 얻으면 true값을 넣어줌
+                go.SetActive(false); // 아이템을 가지고 간 것처럼 보이기 위해 아이템 오브젝트를 화면상에서 그리지 않음
+                UnityEngine.Debug.Log(items[int.Parse(go.name)]);
             }
-            else if (col.gameObject.CompareTag("useitem")) //아이템이 아니고 아이템과 상호작용을 하는 오브젝트(태그가 useitem)일 경우
+            else if (go.CompareTag("useitem")) //아이템이 아니고 아이템과 상호작용을 하는 오브젝트(태그가 useitem)일 경우
             {
-                if (col.gameObject.GetComponent<State>().use == false)
+
+                if (items[go.GetComponent<State>().itemnum] == true) // 그 아이템을 소지하고 있다면.
                 {
-                    col.gameObject.GetComponent<State>().use = true; // 아이템을 사용한 상태로 바꿔줌
-                    UnityEngine.Debug.Log("아이템 사용완료");
+                    if (go.GetComponent<State>().use == false)
+                    {
+                        go.GetComponent<State>().use = true; // 아이템을 사용한 상태로 바꿔줌
+                        UnityEngine.Debug.Log("아이템 사용완료");
+                    }
+                    else
+                    {
+                        UnityEngine.Debug.Log("이미 아이템을 사용했다.");
+                    }
                 }
                 else
-               {
-                   UnityEngine.Debug.Log("이미 아이템을 사용했다.");
-               }
+                {
+                    UnityEngine.Debug.Log("해당 아이템이 없음.");
+                }
+
             }
         }
 
--- Assets/Script/State.cs
+++ Assets/Script/State.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class State : MonoBehaviour {
 
     public bool use = false; // 기본값을 false로 두어 아이템을 사용하지 않은 상태로 둔다.
+    public int itemnum; //어떤 아이템을 사용했을 경우 상태를 바꾸는 가에 대해 필요한 아이템 넘버
 
 	// Use this for initialization
 	void Start () {
